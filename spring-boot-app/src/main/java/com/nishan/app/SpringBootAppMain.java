8d0f09e0ab6e073a472a51bb5cdaee3dcbe36986
nishan <nishancse@gmail.com>
2018-08-14 16:42:40 +0600

    added missing ifl3s

---
+++ spring-boot-app/src/main/java/com/nishan/app/SpringBootAppMain.java
@@ -0,0 +1,12 @@
+package com.nishan.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppMain {
+    public static void main(String args[]){
+        SpringApplication.run(SpringBootAppMain.class,args);
+    }
+
+
+}
